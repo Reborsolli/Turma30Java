16cf5918ba64d72b933ba2c07369f4c14b01ea4b
Reborsolli <renata.ferreirapn@gmail.com>
2021-08-26 16:06:31 -0300

    Spring 3

--- Spring/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ Spring/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -1,36 +1,42 @@
 package org.generation.blogPessoal.model;
 
-
-
 import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "postagem")
 
 public class Postagem {
-	 
-	@Id 
+
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id ;
-	
+	private long id;
+
 	@NotNull
-	@Size(min = 5 , max = 100)
-	private String titulo  ;
-	
+	@Size(min = 5, max = 100)
+	private String titulo;
+
 	@NotNull
-	@Size(min = 5 , max = 500)
-	private String texto ; 
-	
+	@Size(min = 5, max = 500)
+	private String texto;
+
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
 	public long getId() {
 		return id;
@@ -63,5 +69,10 @@ public class Postagem {
 	public void setData(Date data) {
 		this.data = data;
 	}
-
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 }
